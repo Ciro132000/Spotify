d8d9c1306ecdabddd79c42d89c9036fa16797b06
Ciro132000 <c.g_ciro@hotmail.com>
2022-06-05 18:00:21 -0500

    Update 15/06/2022

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api:'http://localhost:3001/api/1.0'
+  // api:'http://localhost:3001/api/1.0',
+  api:'https://musica-back.herokuapp.com/api/1.0'
 };
